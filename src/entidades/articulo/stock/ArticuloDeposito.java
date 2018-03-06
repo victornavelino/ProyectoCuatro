98657f08982b3d7aee1d134d4361b32e816ac8f2
rafatrossero <rafael.trossero@gmail.com>
2018-03-06 13:01:26 -0300

    Modificaciones en entidad articuloDeposito: Se cambió el GenerationType de IDENTITY a AUTO

--- src/entidades/articulo/stock/ArticuloDeposito.java
+++ src/entidades/articulo/stock/ArticuloDeposito.java
@@ -24,7 +24,7 @@ public class ArticuloDeposito implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @ManyToOne
     private Articulo articulo;
